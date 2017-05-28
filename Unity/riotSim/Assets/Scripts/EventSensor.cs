c481b70d072af2a55b63ce5d3dff0e1047cf8687
SimonJ <sjg777@gmail.com>
2017-05-28 15:00:29 -0700

    Event and Stream sensors

--- Unity/riotSim/Assets/Scripts/EventSensor.cs
+++ Unity/riotSim/Assets/Scripts/EventSensor.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class BinarySensor : MonoBehaviour {
-	private string baseURL = "http://127.0.0.1:8080/homeids/";
+public class EventSensor : MonoBehaviour {
+	public string baseURL = "http://127.0.0.1:8080/homeids/";
 
 	public string houseName;
 	public string sensorName;
@@ -13,46 +13,46 @@ public class BinarySensor : MonoBehaviour {
 	public Material offMaterial;
 
 	private MeshRenderer meshRenderer;
-	private bool state;
+	private bool sensorValue;
 	private int frameCnt;
 
 	private UnityWebRequest sensorPost;
 
 	// Use this for initialization
 	void Start () {
-		state = false;
+		sensorValue = false;
 		frameCnt = 0;
 
 		meshRenderer = gameObject.GetComponent <MeshRenderer>();
-		meshRenderer.material = state ? onMaterial : offMaterial;
+		meshRenderer.material = sensorValue ? onMaterial : offMaterial;
 		Debug.LogWarningFormat ("material={0}", meshRenderer.materials[0].ToString());
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (frameCnt % 100 == 0) {
-			Debug.LogFormat ("<{0}, {1}, {2}>", houseName, sensorName, state);
+			//Debug.LogFormat ("<{0}, {1}, {2}>", houseName, sensorName, sensorValue);
 		}
 		frameCnt++;
 	}
 
 	public void Set() {
-		state = !state;
-		Debug.LogWarningFormat ("set={0}", state);
-		meshRenderer.material = state ? onMaterial : offMaterial;
+		sensorValue = !sensorValue;
+		Debug.LogWarningFormat ("set {0}={1}", sensorName, sensorValue);
+		meshRenderer.material = sensorValue ? onMaterial : offMaterial;
 		StartCoroutine (UpdateCloud ());
 	}
 
 	private IEnumerator UpdateCloud() {
-		// TODO send info to cloud
-		sensorPost = UnityWebRequest.Get(baseURL + houseName + "/binary/sensornames/" + sensorName + "/" + state);
-		Debug.LogFormat ("ToCloud: <{0}, {1}, {2}>", houseName, sensorName, state);
+
+		sensorPost = UnityWebRequest.Get(baseURL + houseName + "/event/sensornames/" + sensorName + "/" + sensorValue);
+		Debug.LogFormat ("ToCloud: event <{0}, {1}, {2}>", houseName, sensorName, sensorValue);
 		yield return sensorPost.Send ();
 
 		if (sensorPost.isError) {
 			Debug.Log (sensorPost.error);
 		} else {
-			Debug.Log ("sensorPost logged");
+			Debug.Log ("EventSensor posted");
 		}
 	}
 }
---
+++ Unity/riotSim/Assets/Scripts/StreamSensor.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class StreamSensor : MonoBehaviour {
+	public string baseURL = "http://127.0.0.1:8080/homeids/";
+
+	public string houseName;
+	public string sensorName;
+	public string sampleValue;
+
+	[Tooltip("Sample period in seconds.")]
+	public int samplePeriod = 10;
+
+	private DateTime epochStart;
+	private long lastPostTime;
+	private int frameCnt;
+	private UnityWebRequest sensorPost;
+
+	// Use this for initialization
+	void Start () {
+		epochStart = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+		lastPostTime = 0;
+		frameCnt = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (frameCnt % 100 == 0) {
+			//Debug.LogFormat ("<{0}, {1}, {2}>", houseName, sensorName, sampleValue);
+		}
+		frameCnt++;
+
+		long now = getCurrentSeconds ();
+		if (now - lastPostTime > samplePeriod) {
+			lastPostTime = now;
+			StartCoroutine (UpdateCloud ());
+		}
+	}
+
+	public void Set(string newSampleValue) {
+		sampleValue = newSampleValue;
+		Debug.LogWarningFormat ("set {0}={1}", sensorName, sampleValue);
+	}
+
+	private IEnumerator UpdateCloud() {
+		// TODO send info to cloud
+		sensorPost = UnityWebRequest.Get(baseURL + houseName + "/stream/sensornames/" + sensorName + "/" + sampleValue);
+		Debug.LogFormat ("ToCloud: stream <{0}, {1}, {2}>", houseName, sensorName, sampleValue);
+		yield return sensorPost.Send ();
+
+		if (sensorPost.isError) {
+			Debug.Log (sensorPost.error);
+		} else {
+			Debug.Log ("StreamSensor posted");
+		}
+	}
+
+	private long getCurrentSeconds() {
+		return (long) (DateTime.UtcNow - epochStart).TotalSeconds;
+	}
+}
