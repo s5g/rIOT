b0d6f5964bba6d03efae6a41a8bcb1713102ec08
Devin <itsnotafake@gmail.com>
2017-05-28 14:09:51 -0700

    feat: Changes to Stove script file

--- Unity/riotSim/Assets/Scripts/BinarySensor.cs
+++ Unity/riotSim/Assets/Scripts/BinarySensor.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class BinarySensor : MonoBehaviour {
+	private string baseURL = "http://127.0.0.1:8080/homeids/";
 
 	public string houseName;
 	public string sensorName;
@@ -14,6 +16,8 @@ public class BinarySensor : MonoBehaviour {
 	private bool state;
 	private int frameCnt;
 
+	private UnityWebRequest sensorPost;
+
 	// Use this for initialization
 	void Start () {
 		state = false;
@@ -23,7 +27,7 @@ public class BinarySensor : MonoBehaviour {
 		meshRenderer.material = state ? onMaterial : offMaterial;
 		Debug.LogWarningFormat ("material={0}", meshRenderer.materials[0].ToString());
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (frameCnt % 100 == 0) {
@@ -36,11 +40,19 @@ public class BinarySensor : MonoBehaviour {
 		state = !state;
 		Debug.LogWarningFormat ("set={0}", state);
 		meshRenderer.material = state ? onMaterial : offMaterial;
-		UpdateCloud ();
+		StartCoroutine (UpdateCloud ());
 	}
 
-	private void UpdateCloud() {
+	private IEnumerator UpdateCloud() {
 		// TODO send info to cloud
+		sensorPost = UnityWebRequest.Get(baseURL + houseName + "/binary/sensornames/" + sensorName + "/" + state);
 		Debug.LogFormat ("ToCloud: <{0}, {1}, {2}>", houseName, sensorName, state);
+		yield return sensorPost.Send ();
+
+		if (sensorPost.isError) {
+			Debug.Log (sensorPost.error);
+		} else {
+			Debug.Log ("sensorPost logged");
+		}
 	}
 }
