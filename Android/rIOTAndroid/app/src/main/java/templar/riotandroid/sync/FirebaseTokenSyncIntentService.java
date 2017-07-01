e386e57feb90028b89148fc369e947db04ac9309
Devin <itsnotafake@gmail.com>
2017-07-01 14:34:13 -0700

    feat: userFlow post
    Added (untested) post call to userFlow which is supposed to add the firebase token of the device to endless-shadows user (userFlow) table for communicating with Firebase Cloud Messaging

--- Android/rIOTAndroid/app/src/main/java/templar/riotandroid/sync/FirebaseTokenSyncIntentService.java
+++ Android/rIOTAndroid/app/src/main/java/templar/riotandroid/sync/FirebaseTokenSyncIntentService.java
@@ -1,15 +1,25 @@
 package templar.riotandroid.sync;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.util.Log;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import templar.riotandroid.R;
 
@@ -25,23 +35,46 @@ public class FirebaseTokenSyncIntentService extends IntentService {
     }
 
     @Override
-    protected void onHandleIntent(Intent intent){
-        final RequestQueue requestQueue = Volley.newRequestQueue(this);
-        String tokenUpdateUrl = getString(R.string.endless_shadow_base) +
-                getString(R.string.endless_shadow_create_token);
+    protected void onHandleIntent(Intent intent) {
+        SharedPreferences sharedPreferences = this.getSharedPreferences(
+                getString(R.string.preference_file_key),
+                Context.MODE_PRIVATE);
+        final String firebase_token = sharedPreferences.getString(getString(R.string.firebase_token), "null");
+        if (firebase_token.equals("null")) {
+            Log.e(TAG, "Null firebase token");
 
-        final StringRequest stringRequest2 = new StringRequest(Request.Method.POST,
-                tokenUpdateUrl, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Log.e(TAG, "token successfully added on endless-shadow");
+        } else {
+            JSONObject params = new JSONObject();
+            try{
+                params.put("firebaseToken", firebase_token);
+            }catch(JSONException e){
+                e.printStackTrace();
             }
-        }, new Response.ErrorListener(){
-            @Override
-            public void onErrorResponse(VolleyError error){
-                Log.e(TAG, "Error at getDataUrl: " + error);
-            }
-        });
+
+            final RequestQueue requestQueue = Volley.newRequestQueue(this);
+            String tokenUpdateUrl = getString(R.string.endless_shadow_base) +
+                    getString(R.string.endless_shadow_create_token);
+
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
+                    tokenUpdateUrl, params, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    Log.e(TAG, "token successfully added on endless-shadow");
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e(TAG, "Error at tokensync: " + error);
+                }
+            }) {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    HashMap<String, String> headers = new HashMap<String, String>();
+                    headers.put("Content-Type", "application/json; charset=utf-8");
+                    return headers;
+                }
+            };
+            requestQueue.add(jsonObjectRequest);
+        }
     }
-
 }
